2a9ec8ff626ac6c464034f38345a25dc62174bd6
meinayatraza <me.inayatraza@gmail.com>
2024-10-04 10:46:29 +0500

    Navbar added

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name= "home"),
+    path("", views.base, name="base"),
+    path('index/', views.index, name= "home"),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
+def base(request):
+    return render(request, 'base.html')
+
+def index(request):
     return render(request, 'index.html')
