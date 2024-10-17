b166cd27a8196a41584eb41e4467ed3bfd8786a1
meinayatraza <me.inayatraza@gmail.com>
2024-10-17 09:48:08 +0500

    Account app added

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -18,6 +19,7 @@ class Tweet(models.Model):
 
 
 class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     recipe_name = models.CharField(max_length=50)
     recipe_description = models.TextField()
     recipe_image = models.ImageField(upload_to= "images")
